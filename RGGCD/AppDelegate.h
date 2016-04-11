15a30e4f1c2c5b35de8f2735592f4d7f642f4001
Smartech <rain2540@foxmail.com>
2016-04-11 10:03:53 +0800

    Add files

---
+++ RGGCD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RGGCD
+//
+//  Created by RAIN on 16/4/11.
+//  Copyright © 2016年 Smartech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
