452e261fbcbf32e620f1212e36cde20e0707afc5
Smartech <rain2540@foxmail.com>
2019-06-19 09:09:52 +0800

    feat (RGDispatchSemaphore) : add RGDispatchSemaphore

---
+++ RGGCD/RGGCD/RGDispatchSemaphore.h
@@ -0,0 +1,17 @@
+//
+//  RGDispatchSemaphore.h
+//  RGGCD
+//
+//  Created by RAIN on 2019/6/19.
+//  Copyright © 2019 Smartech. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RGDispatchSemaphore : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ RGGCD/RGGCD/RGDispatchSemaphore.m
@@ -0,0 +1,13 @@
+//
+//  RGDispatchSemaphore.m
+//  RGGCD
+//
+//  Created by RAIN on 2019/6/19.
+//  Copyright © 2019 Smartech. All rights reserved.
+//
+
+#import "RGDispatchSemaphore.h"
+
+@implementation RGDispatchSemaphore
+
+@end
