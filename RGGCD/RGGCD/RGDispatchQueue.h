e3003fe6b3e853b1123a2f6d06bccc9702838192
Smartech <rain2540@foxmail.com>
2019-07-11 08:16:01 +0800

    docs (RGDispatchQueue) : add a comment

--- RGGCD/RGGCD/RGDispatchQueue.h
+++ RGGCD/RGGCD/RGDispatchQueue.h
@@ -119,6 +119,11 @@ typedef NS_ENUM(NSUInteger, DispatchQueueType) {
  @param performance 要提交到目标调度队列的 block, 此参数不能为 NULL
  */
 + (void)performInUtilityGlobalQueue:(dispatch_block_t)performance;
+/**
+ 在调度队列上提交一个异步执行、优先级为 QOS_CLASS_BACKGROUND 的 block, 并且立即返回
+
+ @param performance 要提交到目标调度队列的 block, 此参数不能为 NULL
+ */
 + (void)performInBackgroundGlobalQueue:(dispatch_block_t)performance;
 + (void)performInUserInitiatedGlobalQueue:(dispatch_block_t)performance;
 + (void)performInUnspecifiedGlobalQueue:(dispatch_block_t)performance;
